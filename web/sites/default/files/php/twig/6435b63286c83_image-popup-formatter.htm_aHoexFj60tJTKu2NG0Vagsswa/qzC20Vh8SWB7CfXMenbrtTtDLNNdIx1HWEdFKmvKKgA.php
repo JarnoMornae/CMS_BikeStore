030d2fdf907a3a574a0a5a595a8fa5efde038ffe
Jarno <jarno.mornae@student.hogent.be>
2023-07-05 13:11:18 +0200

    website ok
    todo: css finaliseren

---
+++ web/sites/default/files/php/twig/6435b63286c83_image-popup-formatter.htm_aHoexFj60tJTKu2NG0Vagsswa/qzC20Vh8SWB7CfXMenbrtTtDLNNdIx1HWEdFKmvKKgA.php
@@ -0,0 +1,98 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/image_popup/templates/image-popup-formatter.html.twig */
+class __TwigTemplate_2780e7a088b5e0cbcf786234cad33901 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div class=\"row\">
+    <img src=\"";
+        // line 2
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["url_popup"] ?? null), 2, $this->source), "html", null, true);
+        echo "\"></img>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/image_popup/templates/image-popup-formatter.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  42 => 2,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<div class=\"row\">
+    <img src=\"{{url_popup}}\"></img>
+</div>
+", "modules/contrib/image_popup/templates/image-popup-formatter.html.twig", "C:\\xampp\\htdocs\\bikeStore\\web\\modules\\contrib\\image_popup\\templates\\image-popup-formatter.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 2);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
